48b32946b3a6af94c661288e3726ffc651ad6ed4
Jaafar <jaafaradegoke@gmail.com>
2026-01-20 01:44:21 -0700

    created week-2 folder

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Adegoke Jaafar</h1>
+      <Link href="/">Github</Link>
+      
+    </main>
+  );
+}
