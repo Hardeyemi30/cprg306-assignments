24f85a7f14977ae8776e5a4cba1ea41f1263524e
Jaafar <jaafaradegoke@gmail.com>
2026-01-21 01:46:04 -0700

    assignment completed

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <><p className="text-center text-color">CPRG 306: Web Development 2 - Assignments</p>
-    <Link href="week-2">Go to week-2</Link></>
+          <><h1>Shopping List</h1>
+          <><p className="">CPRG 306: Web Development 2 - Assignments</p>
+      <Link href="week-2">Go to week-2</Link></></>
   );
 }
