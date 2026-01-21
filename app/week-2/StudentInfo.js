061bd248a1bb9f8daf0b2280461df59bce9bce3a
Jaafar <jaafaradegoke@gmail.com>
2026-01-21 01:59:12 -0700

    assignment completed

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <main className="text-white">
-      <h1 className="text-white">Shopping List</h1>
+      <h1>Shopping List</h1>
       <h1>Adegoke Jaafar</h1>
       <Link href="https://github.com/Hardeyemi30/cprg306-assignments">Github</Link>
       
