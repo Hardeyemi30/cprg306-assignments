23e29249651255d791242b44f050e1a5452ac281
Jaafar <jaafaradegoke@gmail.com>
2026-02-02 23:56:34 -0700

    week 4 assignment done

--- app/week-4/item.js
+++ app/week-4/item.js
@@ -1,9 +1,9 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="bg-white shadow-md rounded-lg p-4 mb-3 border border-gray-200">
-      <h2 className="text-lg font-semibold text-gray-800">{name}</h2>
-      <p className="text-gray-600">Quantity: {quantity}</p>
-      <p className="text-sm text-gray-500">Category: {category}</p>
+    <li className="bg-white  p-4 mb-3 border border-green-200">
+      <h2 className="text-lg font-semibold text-green-800">{name}</h2>
+      <p className="text-blue-600">Quantity: {quantity}</p>
+      <p className="text-sm text-green-500">Category: {category}</p>
     </li>
   );
 }
