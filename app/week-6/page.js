8db30c9151a1bf97fe1eec900f35658375d6da18
Jaafar <jaafaradegoke@gmail.com>
2026-02-23 10:51:23 -0700

    week 6 added

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -8,16 +8,13 @@ import itemsData from "../week-4/items.json";
 export default function Page() {
   const [items, setItems] = useState(itemsData || []);
 
-
   const handleAddItem = (newItem) => {
-    setItems([...items, newItem]);
+    setItems((prevItems) => [...prevItems, newItem]);
   };
 
   return (
     <main className="p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">
-        Shopping List
-      </h1>
+      <h1 className="text-3xl font-bold text-center mb-6">Shopping List</h1>
 
       <NewItem onAddItem={handleAddItem} />
 
